6caaf78428ab3a7dcd15688106fe41c3e827c728
inesmcferreira <inesmcferreira@gmail.com>
2021-08-16 21:20:29 +0100

    Add icons to Forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -20,12 +20,15 @@ export default function WeatherForecastDay(props){
         return days[day];
     }
 
-
+console.log(props);
 return (
     <div>
      <div className="weatherForecast-day">{day()}</div>
-             ICON
-           <div className="weatherForecast-temp">
+      <img
+              src={`https://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
+              className="fas fa-sun day-icon"
+            ></img>      
+     <div className="weatherForecast-temp">
           <span className="weatherForecast-temp-max">{maxTemp()} | </span> 
           <span className="weatherForecast-temp-min">{minTemp()}  </span>
            </div> 
